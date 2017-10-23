1ab5996b6433ecd7044be877d3f4cb578573c3b1
Arnaud Plumez <arnaud.plumez@he-arc.ch>
2017-10-23 17:55:24 +0200

    Do Presentation, Parcours & Compétences.
    First version working. It still lacks of ScrollSpy and the section Travaux.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,34 @@
 var $ = require('jquery');
-$(document).ready(() => {
+
+$(document).ready(function () {
+    // Met en place les paramètres des progress-bars
+    var colorClasses = ['success', 'info', 'warning', 'danger'];
+
+    $('.progress-bar').each(function () {
+        let percent = $(this).attr('aria-valuenow') + '%';
+        $(this).css('width', percent);
+        // Pour garantir l'affichage des petits pourcentages (même s'il ne devrait pas y en avoir dans mes compétences ;) )
+        $(this).css('min-width', '2em');
+        $(this).text(percent);
+        // Ajoute une classe changeant la couleur au hasard
+        $(this).addClass('progress-bar-' + colorClasses[Math.floor(Math.random() * colorClasses.length)]);
+    });
+
+    // Scrollspy
+    /*
+    $('.section-title').each(function () {
+        let that = this;
+        let id = 'presentation';
+
+        scrollspy.add(that, {
+            scrollIn: function () {
+                addActiveToMenuItemReferingId(id);
+            }
+        });
+    });
+
+    function addActiveToMenuItemReferingId (id) {
+        $('.navbar-nav li').find('a[href$="' + id + '"]').parent().addClass('active');
+    }
+    */
 });
