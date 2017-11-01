7ccb9eb2ece683aab6317e9b87b5a21c9846f538
Arnaud Plumez <arnaud.plumez@he-arc.ch>
2017-11-01 10:52:52 +0100

    Add Chart.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './vendor/timeline.css';
 import './style.css';
 
 import 'jquery-smooth-scroll';
+import Chart from 'chart.js';
 
 $(document).ready(() => {
     // Met en place les paramètres des progress-bars
@@ -23,5 +24,29 @@ $(document).ready(() => {
 
     // Smooth scroll
     $('a').smoothScroll();
+
+    // Si js est activé, on remplace toutes les progress par ChartJS
+    const chartTag = '<canvas class="progress-chartjs"></canvas>';
+    let charts;
+    let colors = ['red', 'green', 'yellow', 'purple', 'blue'];
+    $('.progress').each(function () {
+        $(this).hide();
+        $(this).parent().append(chartTag);
+
+        let data = [];
+        data.push(parseInt($(this).find('div').attr('aria-valuenow')));
+        data.push(100 - data[0]); // Ajoute la valeur qui manque pour faire 100%
+
+        charts = new Chart($(this).parent().find('.progress-chartjs'), {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: data,
+                    backgroundColor: [colors[Math.floor(Math.random() * colorClasses.length)], 'transparent']
+                }]
+            }
+        });
+    });
+    console.log(charts);
 });
 
